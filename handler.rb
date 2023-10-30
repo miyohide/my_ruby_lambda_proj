5f86835e04cf22ee973b3034069c3c4d0eba52ab
Hidenori Miyoshi <miyohide@gmail.com>
2023-10-30 20:18:42 +0900

    update application

--- handler.rb
+++ handler.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
-require 'json'
+require "json"
+require "date"
 
 def handler(event:, context:)
-  { event: JSON.generate(event), context: JSON.generate(context.inspect) }
+  events = JSON.parse(event.body)
+  status_code = 200
+  case events
+  when "date"
+    result = { date: JSON.generate(Date.today) }
+  when "time"
+    result = { time: JSON.generate(Time.now) }
+  else
+    status_code = 400
+    result = { "error" => "Must specify 'date' or 'time'" }
+  end
+
+  {
+    statusCode: status_code,
+    body: result
+  }
 end
